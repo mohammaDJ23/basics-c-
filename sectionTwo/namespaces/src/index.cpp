0285f3dc1f449242dae07787fc3c14f70ca8e422
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-01-08 17:12:24 +0330

    using namespances

---
+++ sectionTwo/namespaces/src/index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+// use the entire std namespace. this is not a good solution for a big project.
+using namespace std; 
+
+// only use what you want
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+  return 0;
+}
