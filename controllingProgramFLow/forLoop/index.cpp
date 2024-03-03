7df91d49318417b75d9c440e819968c29487f27b
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-03-03 08:10:06 +0330

    adding forLoop

---
+++ controllingProgramFLow/forLoop/index.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  for (int i {1}; i <= 10; i++) 
+    for (int j {1}; j <= 10; j++) 
+      std::cout << i << " * " << j << " = " << i * j << std::endl;
+
+  std::cout << std::endl;
+
+  for (int i {1}, j {6}; i <= 5; i++, j++)
+    std::cout << i << " + " << j << " = " << i + j << std::endl;
+
+  std::cout << std::endl;
+  
+  std::vector<int> vec {10, 20, 30, 40, 50};
+
+  for (unsigned int i {}; i < vec.size(); i++)
+    std::cout << vec.at(i) << std::endl;
+
+  return 0;
+}
