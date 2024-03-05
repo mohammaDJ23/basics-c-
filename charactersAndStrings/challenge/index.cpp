6b297063c6bff1013c904ea0fa4f6db2581222e0
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-03-05 08:55:18 +0330

    revising the challenge logic

--- charactersAndStrings/challenge/index.cpp
+++ charactersAndStrings/challenge/index.cpp
@@ -15,36 +15,32 @@ int main()
   std::cout << "Enter your message: ";
   std::getline(std::cin, message);
 
-  std::cout << "Encrypting is processing..." << std::endl;
-  std::string encrypted_message {};
+  std::cout << std::endl << "Encrypting and decrypting is processing..." << std::endl << std::endl;
+
   std::vector<size_t> alphabet_indexes {};
-  size_t index {};
+  std::vector<size_t> key_indexes {};
+  int ran_num {};
 
-  for (auto chr : message) {
-    index = alphabet.find(chr);
+  for (size_t i {0}; i < message.length(); i++) {
+    key_indexes.push_back(distr(gen));
+    alphabet_indexes.push_back(alphabet.find(message[i]));
+  }
 
-    if (index != std::string::npos) {
-      encrypted_message += key.at(distr(gen));
-      alphabet_indexes.push_back(index);
+  std::string encrypted_message {};
+  std::string decrypted_message {};
+
+  for (size_t i {0}; i < message.length(); i++) {
+    encrypted_message += key.at(key_indexes.at(i));
+
+    if (alphabet_indexes.at(i) != std::string::npos) {
+      decrypted_message += alphabet.at(alphabet_indexes.at(i));
     } else {
-      encrypted_message += chr;
-      alphabet_indexes.push_back(-1);
+      decrypted_message += message.at(i);
     }
   }
 
-  std::cout << "Encrypted message is: " << encrypted_message << std::endl;
-
-  std::cout << "Decrypting is processing..." << std::endl;
-  std::string decrypted_message {};
-
-  for (size_t i {0}; i < alphabet_indexes.size(); i++) {
-    if (alphabet_indexes.at(i) != -1)
-      decrypted_message += alphabet.at(alphabet_indexes.at(i));
-    else
-      decrypted_message += encrypted_message.at(i);
-  }
-
-  std::cout << "Decrypted message is: " << decrypted_message << std::endl;
+  std::cout << "Encrypted message is: " << encrypted_message << std::endl << std::endl;
+  std::cout << "Decrypted message is: " << decrypted_message << std::endl << std::endl;
 
   return 0;
 }
