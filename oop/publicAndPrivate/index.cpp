005bd7989aee1469500a32e8c2d4f8f87985c8cf
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-03-10 07:27:07 +0330

    adding publicAndPrivate

---
+++ oop/publicAndPrivate/index.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+class Player 
+{
+private:
+  std::string name {"Player"};
+  int health {};
+  int xp {};
+
+public:
+  bool is_dead()
+  {
+    return health <= 0;
+  }
+
+  void talk(std::string text)
+  {
+    std::cout << name << " says " << text << std::endl;
+  }
+};
+
+int main()
+{
+  Player person;
+
+  // person.name = "Frank"; // error
+  // person.health = 100; // error
+  // person.xp = 3; // error
+  // std::cout << person.name << std::endl; // error
+
+  person.is_dead();
+  person.talk("hey!!");
+
+  return 0;
+}
