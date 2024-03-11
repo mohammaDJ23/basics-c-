9790e23edde155a242c8ee070ccebf7a011a15de
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-03-11 10:07:04 +0330

    adding copyConstructor

---
+++ oop/copyConstructor/index.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+class Account
+{
+private:
+  std::string name;
+  std::string type;
+  double balance;
+
+public:
+  std::string get_name(void) { return name; }
+  std::string get_type(void) { return type; }
+  double get_balance(void) { return balance; }
+
+  // constructor
+  Account(std::string n = "Unknown", std::string t = "USD", double b = 0.0);
+
+  // copy constructor
+  Account(const Account &source);
+
+  // destructor
+  ~Account();
+};
+
+Account::Account(std::string n, std::string t, double b)
+  : name{n}, type{t}, balance{b} 
+{
+  std::cout << name << " is initializing the constructor." << std::endl;
+}
+
+Account::Account(const Account &source)
+  : name{source.name}, type{source.type}, balance{source.balance} 
+{
+  std::cout << name << " is copying the constructor." << std::endl;
+}
+
+Account::~Account()
+{
+  std::cout << "Destructoring for " << name << " is called." << std::endl;
+}
+
+void display_player(Account account)
+{
+  std::cout << "name is: " << account.get_name() << std::endl;
+  std::cout << "type is: " << account.get_type() << std::endl;
+  std::cout << "balance is: " << account.get_balance() << std::endl;
+}
+
+Account make_account(std::string name)
+{
+  Account account {name};
+  return account;
+}
+
+int main()
+{
+  Account person1;
+  Account person2 {"Mohammad nowresideh"};
+
+  display_player(person2);
+
+  Account person3;
+
+  person3 = make_account("Jack");
+
+  display_player(person3);
+
+  return 0;
+}
