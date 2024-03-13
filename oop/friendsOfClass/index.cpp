a53628f51595f2820739f965c521d3d67501e7de
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-03-13 17:49:13 +0330

    adding friendsOfClass

---
+++ oop/friendsOfClass/index.cpp
@@ -0,0 +1,26 @@
+/*
+
+  - friends of a class means you can access to the private properties.
+
+  - a function or class that has access to private class member,
+    that function or class is not a member of the calss it is accessing.
+
+  - function
+    - can be regular non-member functions.
+    - can be member of another class.
+
+  - class
+    - another calss can have access to private class members.
+    - if A is a friend of B, B is not a friend of A.
+    - if A is a friedn of B and B is a friend of C then A is not a friend of C.
+
+  - more working on this section at the future.
+
+*/
+
+#include <iostream>
+
+int main()
+{
+  return 0;
+}
