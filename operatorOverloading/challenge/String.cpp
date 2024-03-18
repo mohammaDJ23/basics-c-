7c44a71eaa8f47e1ed2f7288497cba1366b3b556
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-03-18 22:02:06 +0330

    adding the basics of the challange

---
+++ operatorOverloading/challenge/String.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+#include "String.h"
+
---
+++ operatorOverloading/challenge/String.h
@@ -0,0 +1,33 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+class String
+{
+private:
+  char *str;
+
+public:
+  String();
+  String(const char *const str);
+  String(const String &source);
+  String(String &&source);
+  ~String();
+
+  String &operator=(const String &obj);
+  String &operator=(String &&obj);
+  String operator-() const;
+  bool operator==(const String &rhs) const;
+  bool operator!=(const String &rhs) const;
+  bool operator<(const String &rhs) const;
+  bool operator>(const String &rhs) const;
+  String operator++() const;
+  String operator++(int) const;
+  String operator--() const;
+  String operator--(int) const;
+  String operator+(const String &rhs) const;
+  String operator+=(const String &rhs) const;
+  String operator*(const String &rhs) const;
+  String operator*=(const String &rhs) const;
+};
+
+#endif
