260f7c450d2249912cdc9c54b92653b69c23d5b1
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2024-03-27 13:23:02 +0330

    adding multipleException

---
+++ exception/multipleException/index.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+/*
+
+    this function only should return a valid double.
+    but line 13 will not work when gallons is 0.
+    so you have to check for this condition to throw an error.
+    you could not return a valid result.
+
+*/
+double calculate_mpg(int miles, int gallons)
+{
+    if (gallons == 0)
+        throw 0;
+    if (miles < 0 || gallons < 0)
+        throw std::string{"Could not calculate a negitive number."};
+    return static_cast<double>(miles) / gallons;
+}
+
+int main()
+{
+    int miles {};
+    int gallons {};
+    double miles_per_gallon {};
+
+    std::cout << "Enter the miles drives: ";
+    std::cin >> miles;
+    std::cout << "Enter the gallon used: ";
+    std::cin >> gallons;
+
+    try {
+        miles_per_gallon = calculate_mpg(miles, gallons);
+        std::cout << miles_per_gallon << std::endl;
+    } catch(const int &ex) {
+        std::cerr << "Could not divide by zero." << std::endl;
+    } catch(const std::string &ex) {
+        std::cerr << ex << std::endl;
+    } catch (...) {
+        std::cout << "Unhandled catch." << std::endl;
+    }
+    
+    std::cout << "Bye" << std::endl;
+
+    return 0;
+}
