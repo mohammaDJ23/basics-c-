1867034d8352f7a8012c27f368852728e32ca75a
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-05-23 13:59:32 +0330

    adding generate

---
+++ algorithms/modifyingSequenceOperations/generate/index.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template<class ForwardIt, class Generator>
+constexpr //< since C++20
+void generate(ForwardIt first, ForwardIt last, Generator g)
+{
+    for (; first != last; ++first)
+        *first = g();
+}
+ 
+void println(std::string_view fmt, const auto& v)
+{
+    for (std::cout << fmt; const auto& e : v)
+        std::cout << e << ' ';
+    std::cout << '\n';
+};
+ 
+ 
+int f()
+{
+    static int i;
+    return ++i;
+}
+ 
+int main()
+{
+    std::vector<int> v(5);
+ 
+    std::generate(v.begin(), v.end(), f);
+    println("v: ", v);
+ 
+    // Initialize with default values 0,1,2,3,4 from a lambda function
+    // Equivalent to std::iota(v.begin(), v.end(), 0);
+    std::generate(v.begin(), v.end(), [n = 0] () mutable { return n++; });
+    println("v: ", v);
+}
