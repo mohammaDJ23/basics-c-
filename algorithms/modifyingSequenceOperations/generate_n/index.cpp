243cdf37db1e41bff08b5077882b4e438cc495ce
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-05-23 14:14:22 +0330

    adding generate_n

--- algorithms/modifyingSequenceOperations/generate_n/index.cpp
+++ algorithms/modifyingSequenceOperations/generate_n/index.cpp
@@ -13,10 +13,24 @@ OutputIt generate_n(OutputIt first, Size count, Generator g)
  
     return first;
 }
+
+int gen() 
+{ 
+    static int i {0}; 
+    return ++i; 
+} 
  
 int main()
 {
     std::mt19937 rng; // default constructed, seeded with fixed seed
     std::generate_n(std::ostream_iterator<std::mt19937::result_type>(std::cout, " "), 5, std::ref(rng));
     std::cout << '\n';
+
+    std::vector<int> v1(10); 
+    std::generate_n(v1.begin(), v1.size(), gen); 
+    for (std::vector<int>::iterator i1 = v1.begin(); i1 != v1.end(); ++i1) { 
+        std::cout << *i1 << " "; 
+    }
+    std::cout << std::endl;
+    return 0;
 }
